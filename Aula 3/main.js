d7f5244dea7d42560d5a6e48410c42ff735353f0
ecaramujo <80298792+ecaramujo@users.noreply.github.com>
2021-04-15 18:53:46 -0300

    Add files via upload

---
+++ Aula 3/main.js
@@ -0,0 +1,22 @@
+const Produto = require("./produto");
+const CadastroProdutos = require("./cadastro_produtos");
+
+let cadastroProdutos = new CadastroProdutos();
+
+const produto = new Produto(1,"arroz",6.3);
+
+cadastroProdutos.inserir(produto);
+cadastroProdutos.inserir(new Produto(5,"teste",4));
+cadastroProdutos.inserir(new Produto(2,"feijao",8.9));
+console.log("Primeiro");
+for(let prod of cadastroProdutos.listar()){
+    console.log (`Produto ${prod.codigo}: ${prod.nome}, ${prod.preco}`);
+}
+console.log("Segundo");
+console.log(cadastroProdutos.deletar(5));
+console.log("Terceiro");
+cadastroProdutos.atualizar(2, new Produto(2,"feijao",9.2));
+
+console.log(cadastroProdutos.listar());
+
+
